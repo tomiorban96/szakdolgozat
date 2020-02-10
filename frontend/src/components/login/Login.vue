e4ce197f3337856f21c84d07ccbc28aa961ef8b9
Orbán Tamás <tomiorban96@gmail.com>
2020-02-10 12:52:28 +0100

    Add registration form

--- frontend/src/components/login/Login.vue
+++ frontend/src/components/login/Login.vue
@@ -8,23 +8,39 @@
     <v-card>
       <v-form v-model="formIsValid" @submit.prevent="loginClicked">
         <v-card-title class="headline primary headline-text" primary-title>
-          Login
+          {{registerFormIsOpen ? 'Register' : 'Login'}}
           <v-spacer />
-          <v-btn icon @click="showLoginDialog = false" class="close-button" :disabled="loginLoading">
+          <v-btn icon @click="onClose" class="close-button" :disabled="loginLoading">
             <v-icon>mdi-close</v-icon>
           </v-btn>
         </v-card-title>
         <v-card-text>
+          <div>
           <v-text-field :rules="emailRules" v-model="email" required label="Email" name="email"
-            prepend-icon="mdi-account" type="email" />
+            :prepend-icon="registerFormIsOpen ? undefined : 'mdi-account'" type="email" />
           <v-text-field :rules="passwordRules" v-model="password" required id="password" label="Password"
-            name="password" prepend-icon="mdi-lock" type="password" />
+            name="password" :prepend-icon="registerFormIsOpen ? undefined : 'mdi-lock'" type="password" />
+          </div>
+          <div v-if="registerFormIsOpen">
+            <v-text-field :rules="passwordVerifyRules" v-model="verifyPassword" required id="verifyPassword" label="Verify Password"
+            name="verify password" prepend-icon="" type="password" />
+            <v-text-field :rules="nameRules" v-model="firstName" required id="firstName" label="First Name"
+            name="firstname" prepend-icon="" type="text" />
+            <v-text-field :rules="nameRules" v-model="lastName" required id="lastName" label="Last Name"
+            name="lastname" prepend-icon="" type="text" />
+          </div>
         </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
+          <v-btn v-if="!registerFormIsOpen" @click="onRegisterFormOpened" color="primary" text>
+            Register
+          </v-btn>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text :loading="loginLoading" type="submit" :disabled="!formIsValid">
+          <v-btn v-if="!registerFormIsOpen" color="primary" :loading="loginLoading" type="submit" :disabled="!formIsValid">
             Login
+          </v-btn>
+          <v-btn v-else color="primary" :loading="registerLoading" type="submit" :disabled="!formIsValid">
+            Register
           </v-btn>
         </v-card-actions>
       </v-form>
@@ -33,6 +49,8 @@
 </template>
 
 <script>
+  //import auth from '../../requests/auth';
+
   export default {
     props: {
       source: String,
@@ -50,10 +68,37 @@
       formIsValid: false,
       showLoginDialog: true,
       loginLoading: false,
+      cardTitle: "Login",
+      registerFormIsOpen: false,
+      verifyPassword: null,
+      firstName: null,
+      lastName: null,
+      registerLoading: false,
+      nameRules: [
+        v => !!v || 'Field is required'
+      ],
+      passwordVerifyRules: [
+        v => !!v || 'Field is required'
+      ]
     }),
     methods: {
       loginClicked() {
+        /*auth.authenticate(this.email, this.password)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        })*/
         this.loginLoading = !this.loginLoading;
+      },
+      onRegisterFormOpened() {
+        this.registerFormIsOpen = true;
+        this.cardTitle = "Register";
+      },
+      onClose() {
+        this.showLoginDialog = false;
+        this.registerFormIsOpen = false;
       }
     }
   }
